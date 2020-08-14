d0f9e68f055a2ddafca5cf5e5446df483574d158
limasha <limasha.perera.98@gmail.com>
2020-08-14 21:34:04 +0530

    add new files

---
+++ Task_1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number1 = int.Parse(textBox1.Text);
+            int number2 = int.Parse(textBox2.Text);
+
+            int ans = number1 + number2;
+            textBox3.Text = ans.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int number1 = int.Parse(textBox1.Text);
+            int number2 = int.Parse(textBox2.Text);
+
+            int ans = number1 - number2;
+            textBox3.Text = ans.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int number1 = int.Parse(textBox1.Text);
+            int number2 = int.Parse(textBox2.Text);
+
+            int ans = number1 * number2;
+            textBox3.Text = ans.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int number1 = int.Parse(textBox1.Text);
+            int number2 = int.Parse(textBox2.Text);
+
+            int ans = number1 / number2;
+            textBox3.Text = ans.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox1.Focus();
+        }
+    }
+}
